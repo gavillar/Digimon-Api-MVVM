399a26cd5a4fc34a9904dbfd01b9ba88030d017c
VillarDeveloper <villar.developer@gmail.com>
2023-01-04 12:23:13 -0800

    Initial Commit

---
+++ DigimonAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigimonAPI
+//
+//  Created by user220831 on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
